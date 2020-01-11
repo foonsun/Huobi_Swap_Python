1d8f618add6bd62556452538e6ad2594d3d5c521
foonsun <qiaoxiaofeng@huobi.com>
2020-01-11 18:52:14 +0800

    first commit

---
+++ alpha/const.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+"""
+some constants and global queues
+
+Author: QiaoXiaofeng
+Date:   2020/01/11
+Email:  andyjoe318@gmail.com
+"""
+
+from collections import deque
+
+# Exchange Names
+HUOBI_SWAP = "huobi_swap"  # Huobi Future https://www.hbdm.com/en-us/contract/exchange/
+
+# Market Types
+MARKET_TYPE_TRADE = "trade"
+MARKET_TYPE_ORDERBOOK = "orderbook"
+MARKET_TYPE_KLINE = "kline"
